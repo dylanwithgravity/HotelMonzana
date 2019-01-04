e3425402a24ef7891368249731eecb1ac6d868bd
dylanwithgravity <dylanwithgravity@gmail.com>
2019-01-04 14:03:10 -0500

    Added didSelectRow tableView delegate method to show/hide appropriate date picker

--- HotelMonzana/AddRegistrationTableViewController.swift
+++ HotelMonzana/AddRegistrationTableViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class AddRegistrationTableViewController: UITableViewController {
     
+    // Store index path for pickers for easy comparison
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    // Store whether or not the date pickers will be shown, then show or hide them when the properties are set
+    var isCheckInDatePickerShown: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerShown
+        }
+    }
+    
+    var isCheckOutDatePickerShown: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerShown
+        }
+    }
+    
 
     
     @IBOutlet weak var firstNameTextField: UITextField!
@@ -47,6 +64,7 @@ class AddRegistrationTableViewController: UITableViewController {
         checkOutDateLabel.text = dateFormatter.string(from: checkOutDatePicker.date)
     }
     
+    
     @IBAction func datePickerValueChanged(_ sender: UIDatePicker) {
         updateDateViews()
     }
@@ -67,6 +85,69 @@ class AddRegistrationTableViewController: UITableViewController {
         print("Check-Out date: \(checkOutDate)")
         
         
+    }
+    
+    // MARK: - Table View delegate methods
+    
+    // Dynamically modify the row's height at runtime
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch (indexPath.section, indexPath.row) {
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row):
+            if isCheckInDatePickerShown {
+                return 140.0
+            } else {
+                return 0
+            }
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row):
+            if isCheckOutDatePickerShown {
+                return 140.0
+            } else {
+                return 0
+            }
+        default:
+            return 44.0
+        }
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Deselect cell user taps
+        tableView.deselectRow(at: indexPath, animated: true)
+       
+        // Check if selected cell is date label, if so toggle appropriate date picker
+        switch (indexPath.section, indexPath.row){
+        case (checkInDatePickerCellIndexPath.section, checkInDatePickerCellIndexPath.row - 1):
+            if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+            } else if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+                isCheckInDatePickerShown = true
+            } else {
+                isCheckInDatePickerShown = true
+            }
+            
+            // Action tableView to requery attributes including cell height
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        case (checkOutDatePickerCellIndexPath.section, checkOutDatePickerCellIndexPath.row - 1):
+            if isCheckOutDatePickerShown {
+                isCheckOutDatePickerShown = false
+            } else if isCheckInDatePickerShown {
+                isCheckInDatePickerShown = false
+                isCheckOutDatePickerShown = true
+            } else {
+                isCheckOutDatePickerShown = true
+            }
+            
+            // Action tableView to requery attributes including cell height
+
+            tableView.beginUpdates()
+            tableView.endUpdates()
+            
+        default:
+            break
+        }
     }
     
 
